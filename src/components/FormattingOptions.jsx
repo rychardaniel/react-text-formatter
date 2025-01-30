39d07033c7553de69577efaf9a98d1f2a2e7e570
rychardaniel <164958686+rychardaniel@users.noreply.github.com>
2025-01-30 17:57:36 -0300

    inicio do layout do Formatting Options

--- src/components/FormattingOptions.jsx
+++ src/components/FormattingOptions.jsx
@@ -1,9 +1,14 @@
 import Title from "./Title";
+import YesOrNo from "./YesOrNo";
 
 const FormattingOptions = () => {
     return (
         <div className="bg-neutral-700 w-1/5 rounded-md p-2 text-center">
             <Title>FORMATTING OPTIONS</Title>
+            <div className="flex flex-col gap-6 items-center">
+                <YesOrNo>Accents?</YesOrNo>
+                <YesOrNo>Punctuation?</YesOrNo>
+            </div>
         </div>
     );
 };
